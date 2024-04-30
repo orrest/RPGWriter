c811d0a072bb332867bc8e4c97c4c42d0452df57
xufeng <xufeng@guopaigaoke.com>
2024-04-30 18:47:55 +0800

    Add DI

--- RPGWriter/RPGWriter/App.axaml.cs
+++ RPGWriter/RPGWriter/App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using RPGWriter.Extensions;
 using RPGWriter.ViewModels;
 using RPGWriter.Views;
 
@@ -15,18 +17,27 @@ namespace RPGWriter
 
         public override void OnFrameworkInitializationCompleted()
         {
+            // Register all the services needed for the application to run
+            var collection = new ServiceCollection();
+            collection.AddDependencies();
+
+            // Creates a ServiceProvider containing services from the provided IServiceCollection
+            var services = collection.BuildServiceProvider();
+
+            var vm = services.GetRequiredService<MainViewModel>();
+
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainViewModel()
+                    DataContext = vm
                 };
             }
             else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
             {
                 singleViewPlatform.MainView = new MainView
                 {
-                    DataContext = new MainViewModel()
+                    DataContext = vm
                 };
             }
 
---
+++ RPGWriter/RPGWriter/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RPGWriter.ViewModels;
+
+namespace RPGWriter.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDependencies(this ServiceCollection collection)
+        {
+            AddCommonServices(collection);
+            AddViewModels(collection);
+        }
+
+        private static void AddCommonServices(this IServiceCollection collection)
+        {
+            //collection.AddSingleton<IRepository, Repository>();
+            //collection.AddTransient<BusinessService>();
+        }
+
+        private static void AddViewModels(this IServiceCollection collection)
+        {
+            collection.AddTransient<MainViewModel>();
+        }
+    }
+}
